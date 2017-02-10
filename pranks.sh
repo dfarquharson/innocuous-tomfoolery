af437b2a2f5679a03fe9769ec67b357f2efb0bf6
Anthony Odufuye <toodufuye@spscommerce.com>
2017-02-10 12:21:46 -0600

    no longer printing process id stuff to stdout

--- pranks.sh
+++ pranks.sh
@@ -1,3 +1,3 @@
-alias ls='say "youve made a horrible mistake $USER" & ls'
-alias cd='say "launching the nukes from $HOSTNAME as $USER" & cd'
-alias git='say "emailing your AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY to all your contacts" & git'
+alias ls='(say "youve made a horrible mistake $USER" &); ls 2>/dev/null'
+alias cd='(say "launching the nukes from $HOSTNAME as $USER" &); cd 2>/dev/null'
+alias git='(say "emailing your AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY to all your contacts" &); git 2>/dev/null'
